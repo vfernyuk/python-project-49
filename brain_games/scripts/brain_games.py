bfc8efb1da851ba2da0690baee8ae242c7d82469
Vladislav Fernyuk <fernyuk@gmail.com>
2024-04-13 13:26:22 +0300

    Step3, modified cli.py and brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
+
+from brain_games.cli import welcome_user
+
+
 def main():
-    print(f'Welcome to the Brain Games!')
+    welcome_user()
+
 
 if __name__ == '__main__':
     main()
+
