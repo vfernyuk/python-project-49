643fca02341f30d6b3f153e1d9d64f5d9d6a805a
Vladislav Fernyuk <fernyuk@gmail.com>
2024-04-23 22:39:48 +0300

    step8, new game progression

--- Makefile
+++ Makefile
@@ -13,6 +13,9 @@ brain-calc:
 brain-gcd:
 	poetry run brain-gcd
 
+brain-progression:
+	poetry run brain-progression
+
 install:
 	poetry install
 
---
+++ brain_games/games/brain_progression.py
@@ -0,0 +1,18 @@
+from random import randint
+
+question = 'What number is missing in the progression?'
+
+
+def get_question():
+    first_number_progression = randint(1, 10)
+    step_progression = randint(1, 10)
+    progression = list(range(first_number_progression, 100, step_progression))
+    lenght_progression = 10
+    limited_progression = progression[:lenght_progression]
+    random_index = randint(0, 9)
+    hidden_number = progression[random_index]
+    progression[random_index] = '..'
+    new_task = ' '.join(str, progression)
+    correct_answer = str(hidden_number)
+    return new_task, correct_answer
+ 
