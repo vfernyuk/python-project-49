eee1ae07388077525103ae2db5832986db922f87
Vladislav Fernyuk <fernyuk@gmail.com>
2024-04-25 21:54:04 +0300

    debug game-brain_progression

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -13,7 +13,7 @@ def get_question():
     random_index = randint(0, 9)
     hidden_number = progression[random_index]
     progression[random_index] = '..'
-    new_task = ' '.join(map(str, progression)
+    new_task = ' '.join(map(str, progression))
     correct_answer = str(hidden_number)
     return new_task, correct_answer
  
