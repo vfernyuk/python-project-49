3693342f23b8565f30f1297c5968af030f820925
Vladislav Fernyuk <fernyuk@gmail.com>
2024-04-25 22:02:39 +0300

    debug game-brain_progression

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -7,8 +7,7 @@ question = 'What number is missing in the progression?'
 def get_question():
     first_number_progression = randint(1, 10)
     step_progression = randint(1, 10)
-    progression = list(range(first_number_progression, 100, step_progression))
-    limited_progression = progression[:10]
+    progression = list(range(first_number_progression, 100, step_progression))[:10]
     random_index = randint(0, 9)
     hidden_number = progression[random_index]
     progression[random_index] = '..'
