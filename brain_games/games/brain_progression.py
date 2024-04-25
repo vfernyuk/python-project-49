5ce64fd32e58cbd92786cad824cf26de19547787
Vladislav Fernyuk <fernyuk@gmail.com>
2024-04-25 21:55:45 +0300

    debug game-brain_progression

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -8,7 +8,7 @@ def get_question():
     first_number_progression = randint(1, 10)
     step_progression = randint(1, 10)
     progression = list(range(first_number_progression, 100, step_progression))
-    lenght_progression = 10
+    lenght_progression == 10
     limited_progression = progression[:lenght_progression]
     random_index = randint(0, 9)
     hidden_number = progression[random_index]
