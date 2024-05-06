e5733f01c79ae3e38a72e30dc4d6b34594fb5497
Vladislav Fernyuk <fernyuk@gmail.com>
2024-05-06 23:04:34 +0300

    debug linter errors

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -5,9 +5,9 @@ question = 'What number is missing in the progression?'
 
 
 def get_question():
-    first_number_progression = randint(1, 10)
+    first_number_progres = randint(1, 10)
     step_progression = randint(1, 10)
-    progression = list(range(first_number_progression, 100, step_progression))[:10]
+    progression = list(range(first_number_progres, 100, step_progression))[:10]
     random_index = randint(0, 9)
     hidden_number = progression[random_index]
     progression[random_index] = '..'
